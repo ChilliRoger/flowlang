5b59bbe3a34167bc2a0c97e1d8933f984ecb3762
ChilliRoger <kuttybird12@gmail.com>
2025-12-17 20:02:57 +0530

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="flowlang-script",
-    version="1.0.0",
+    version="1.0.1",
     author="ChilliRoger",
     author_email="your.email@example.com",  # Update this
     description="A lightweight interpreted programming language for workflow automation and API orchestration",
